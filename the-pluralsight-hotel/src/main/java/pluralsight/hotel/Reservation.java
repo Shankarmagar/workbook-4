56be7d744cd45c2698c3c4738726f21ddceacd3a
Shankar Ale Magar <shankaralemagar91@gmail.com>
2023-11-03 14:59:33 -0400

    Done some method

--- the-pluralsight-hotel/src/main/java/pluralsight/hotel/Reservation.java
+++ the-pluralsight-hotel/src/main/java/pluralsight/hotel/Reservation.java
@@ -1,6 +1,8 @@
 package pluralsight.hotel;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 public class Reservation {
@@ -20,6 +22,8 @@ public class Reservation {
     RoomType requestedType;
 
 
+
+
     // The following fields are assigned when the guest checks in
     Room room = null;
     Folio folio = null;
@@ -30,9 +34,56 @@ public class Reservation {
         return requestedType;
     }
 
-    public void setRoomType(String roomtype)
+    public void setRoomType(RoomType roomType)
     {
-        this.requestedType
+        this.requestedType = roomType;
     }
 
+    public double getPrice(RoomType roomType)
+    {
+        double rateForPrice = 124.00;
+        if(roomType == RoomType.King)
+        {
+            rateForPrice = 139.00;
+
+        }
+        return rateForPrice;
+
+    }
+
+    public int getNumberOfNights() {
+        //Compare the start and End
+            return (int) ChronoUnit.DAYS.between(start, end);
+            //https://docs.oracle.com/javase/8/docs/api/java/time/temporal/ChronoUnit.html
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        start = LocalDate.now();
+        end = start.plusDays(numberOfNights);
+        //https://docs.oracle.com/javase/8/docs/api/java/time/LocalDate.html
+    }
+
+    public boolean isWeekend() {
+        int numberOfWeekends = 0;
+        LocalDate currentDate = start;
+
+        while (!currentDate.isAfter(end)) {
+            if (currentDate.getDayOfWeek().toString().equals("Saturday") || currentDate.getDayOfWeek().toString().equals("Sunday")) {
+                numberOfWeekends++;
+            }
+            currentDate = currentDate.plusDays(1);
+        }
+        if (numberOfWeekends>0)
+        {
+            return true;
+        }
+        return false;
+
+    }
+
+    public void setWeekend(boolean weekend) {
+    }
+
+    public int getGetReservationTotal() {
+    }
 }
